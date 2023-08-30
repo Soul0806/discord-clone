d424dc1103b42050e4dc36ea87b717e2997579f4
soul <celestiallie@gmail.com>
2023-08-30 20:20:59 +0800

    update

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,12 +3,12 @@ import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-between">
       <p className="text-3xl font-bold">
         Hello Discord Clone
       </p>
-      <UserButton afterSignOutUrl="/" />
       <ModeToggle />
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
